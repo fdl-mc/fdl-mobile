5bb50f0186c8e773d3167a827537b59690d6d092
Arsenii Liunsha <me@arslee.dev>
2021-08-21 23:14:24 +0900

    chore(showSnackbar): Remove post-callback

--- lib/features/root/utils/showSnackbar.dart
+++ lib/features/root/utils/showSnackbar.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 
 /// Shows snackbar
 void showSnackbar(BuildContext context, SnackBar snackbar) {
-  WidgetsBinding.instance?.addPostFrameCallback((_) {
-    ScaffoldMessenger.of(context)
-      ..hideCurrentSnackBar()
-      ..showSnackBar(snackbar);
-  });
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(snackbar);
 }
