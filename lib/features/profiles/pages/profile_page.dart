2cd09c01e9ae1009b0a2406b06dca51d4e0df854
Arsenii Liunsha <me@arslee.dev>
2021-11-08 10:27:56 +0900

    chore: Minor changes

--- lib/features/profiles/pages/profile_page.dart
+++ lib/features/profiles/pages/profile_page.dart
@@ -1,3 +1,5 @@
+import 'package:fdl_api/fdl_api.dart';
+import 'package:fdl_app/features/auth/misc/providers.dart';
 import 'package:fdl_app/features/profiles/misc/providers.dart';
 import 'package:fdl_app/shared/config.dart';
 import 'package:fdl_app/shared/widgets/spacer.dart';
@@ -19,116 +21,7 @@ class ProfilePage extends ConsumerWidget {
       body: state.when(data: (user) {
         return SafeArea(
           child: ListView(
-            children: [
-              Card(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 12.0,
-                        vertical: 20.0,
-                      ),
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(6.0),
-                            child: Image.network(
-                              'https://minotar.net/helm/${user.nickname}.png',
-                              fit: BoxFit.cover,
-                              width: 36,
-                              height: 36,
-                            ),
-                          ),
-                          const RSpacer(16),
-                          Text(
-                            user.nickname,
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                          const Spacer(),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.payment),
-                              ),
-                              const RSpacer(12.0),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.gavel),
-                              ),
-                              const RSpacer(12.0),
-                              IconButton(
-                                onPressed: () {
-                                  Clipboard.setData(
-                                    ClipboardData(
-                                      text: kBaseUrl.toString() +
-                                          Routemaster.of(context)
-                                              .currentRoute
-                                              .path,
-                                    ),
-                                  ).then(
-                                    (value) {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        const SnackBar(
-                                          content: Text(
-                                            "Profile link copied to clipboard.",
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  );
-                                },
-                                icon: const Icon(Icons.copy),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Card(
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: const SizedBox(
-                        height: double.infinity,
-                        child: Icon(Icons.person),
-                      ),
-                      title: const Text('User ID'),
-                      subtitle: Text(user.id),
-                    ),
-                    ListTile(
-                      leading: const SizedBox(
-                        height: double.infinity,
-                        child: Icon(Icons.people),
-                      ),
-                      title: const Text('Discord ID'),
-                      subtitle: Text(user.discordId),
-                    ),
-                    ListTile(
-                      leading: const SizedBox(
-                        height: double.infinity,
-                        child: Icon(Icons.schedule),
-                      ),
-                      title: const Text('Registered at'),
-                      subtitle: Text(user.createdAt.toString()),
-                    ),
-                    ListTile(
-                      leading: const SizedBox(
-                        height: double.infinity,
-                        child: Icon(Icons.account_balance_wallet),
-                      ),
-                      title: const Text('Balance'),
-                      subtitle: Text(user.balance.toString() + ' PF'),
-                    ),
-                  ],
-                ),
-              )
-            ],
+            children: [_ProfileCard(user), _StatsCard(user)],
           ),
         );
       }, loading: () {
@@ -136,6 +29,131 @@ class ProfilePage extends ConsumerWidget {
       }, error: (err, stack) {
         return Center(child: Text(err.toString()));
       }),
+    );
+  }
+}
+
+class _StatsCard extends StatelessWidget {
+  final User user;
+  const _StatsCard(this.user, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          ListTile(
+            leading: const SizedBox(
+              height: double.infinity,
+              child: Icon(Icons.person),
+            ),
+            title: const Text('User ID'),
+            subtitle: Text(user.id),
+          ),
+          ListTile(
+            leading: const SizedBox(
+              height: double.infinity,
+              child: Icon(Icons.people),
+            ),
+            title: const Text('Discord ID'),
+            subtitle: Text(user.discordId),
+          ),
+          ListTile(
+            leading: const SizedBox(
+              height: double.infinity,
+              child: Icon(Icons.schedule),
+            ),
+            title: const Text('Registered at'),
+            subtitle: Text(user.createdAt.toString()),
+          ),
+          ListTile(
+            leading: const SizedBox(
+              height: double.infinity,
+              child: Icon(Icons.account_balance_wallet),
+            ),
+            title: const Text('Balance'),
+            subtitle: Text(user.balance.toString() + ' PF'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ProfileCard extends StatelessWidget {
+  final User user;
+  const _ProfileCard(this.user, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 12.0,
+              vertical: 20.0,
+            ),
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(6.0),
+                  child: Image.network(
+                    'https://minotar.net/helm/${user.nickname}.png',
+                    fit: BoxFit.cover,
+                    width: 36,
+                    height: 36,
+                  ),
+                ),
+                const RSpacer(16),
+                Text(
+                  user.nickname,
+                  style: const TextStyle(fontSize: 20),
+                ),
+                const Spacer(),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    if (user.id != context.read(currentUserProvider)?.uid) ...[
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.payment),
+                      ),
+                      const RSpacer(12.0),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.gavel),
+                      ),
+                    ],
+                    const RSpacer(12.0),
+                    IconButton(
+                      onPressed: () {
+                        Clipboard.setData(
+                          ClipboardData(
+                            text: kBaseUrl.toString() +
+                                Routemaster.of(context).currentRoute.path,
+                          ),
+                        ).then(
+                          (value) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                  "Profile link copied to clipboard.",
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                      icon: const Icon(Icons.copy),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
