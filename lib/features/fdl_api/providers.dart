aa6144f4c0ae1049e678266012b678c7165d97a4
Arsenii Liunsha <me@arslee.dev>
2021-11-15 21:19:36 +0900

    chore: Update depedencies and smth

--- lib/features/fdl_api/providers.dart
+++ lib/features/fdl_api/providers.dart
@@ -1,6 +1,7 @@
 import 'package:fdl_api/fdl_api.dart';
+import 'package:fdl_app/shared/config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final fdlApiProvider = Provider<FdlApi>(
-  (ref) => FdlApi('calamity34.ddns.net:3000'),
+  (ref) => FdlApi(kApiUrl.host),
 );
--- lib/shared/config.dart
+++ lib/shared/config.dart
@@ -1,1 +1,2 @@
-final kBaseUrl = Uri.parse('http://fdl-mc.ru');
+final kBaseUrl = Uri.parse('https://app.fdl-mc.ru');
+final kApiUrl = Uri.parse('https://api.fdl-mc.ru');
